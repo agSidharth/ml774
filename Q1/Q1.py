742f93fea3028c3cc6a38e2ed4f9bfbdf01f126d
agSidharth <sidharthagarwal2001@gmail.com>
2022-09-03 00:37:03 +0530

    update :)

---
+++ Q1/Q1.py
@@ -0,0 +1,154 @@
+#importing necessary libraries..
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from mpl_toolkits import mplot3d
+
+#loading training data..
+
+dfX = pd.read_csv("/Users/sidharthagarwal/Desktop/assignments/ml774/data/q1/linearX.csv")
+dfY = pd.read_csv("/Users/sidharthagarwal/Desktop/assignments/ml774/data/q1/linearY.csv")
+
+X = dfX.to_numpy()
+Y = dfY.to_numpy()
+
+# adding intercept in X
+X = np.append(X,np.ones(X.shape),axis = 1)
+
+# Note: X is (num_examples*num_features)
+print(X.shape)
+print(Y.shape)
+
+# normalization of input
+mean = np.mean(X[:,0])
+std  = np.std(X[:,0])
+
+X[:,0] = X[:,0]-mean
+X[:,0] = X[:,0]/std
+
+print("Mean: " + str(mean))
+print("STD: "+str(std))
+
+# learning parameter and some more parameters initialization
+alpha = 0.1
+epsilon = 0.0000001
+max_iter = 10000
+theta = np.zeros((X.shape[1],1))
+
+cost_list = []
+theta_list = [theta]
+
+draw_3d = False   # otherwise plot the scatter plot..
+
+if draw_3d:
+    print("Creating the 3d plot")
+else:
+    print("Creating the contour plot")
+
+# helper functions to compute cost, calculate the gradient and train the model..
+# note that np.dot gives amtrix multiplication for 2d arrays..
+
+def cost(X,Y,theta):
+    temp = np.dot(X,theta)
+    temp = (Y - temp)**2
+    this_cost = np.sum(temp)/(2*X.shape[0])
+    return this_cost
+
+def cost_grad_update(X,Y,theta,alpha):
+    temp = np.dot(X,theta)
+    temp = Y - temp
+    theta = theta + (alpha*np.dot(X.transpose(),temp))/(X.shape[0])
+    return theta
+
+def train(X,Y,theta):
+    iteration = 0
+    finished = False
+
+    while(not finished):
+        curr_cost = cost(X,Y,theta)
+        theta = cost_grad_update(X,Y,theta,alpha)
+
+        finished = finished | (iteration>=max_iter)
+        if(iteration>0):
+            finished = finished | (abs(curr_cost - cost_list[-1])<epsilon)
+
+        cost_list.append(curr_cost)
+        theta_list.append(theta)
+
+        if iteration%10==0 and draw_3d:
+            ax.scatter3D(theta[0],theta[1],curr_cost)
+        elif (iteration%10==0):
+            ax.scatter(theta[0],theta[1],marker='x')
+        #plt.pause(0.2)
+        iteration += 1
+    
+    if draw_3d:
+        plt.savefig('../plots/1_c.png')
+    else:
+        plt.savefig('../plots/1_d_'+str(alpha)+'.png')
+    #plt.show()
+    
+    print("Learning rate: "+str(alpha))
+    print("Iterations taken: "+str(iteration))
+    print("Epsilon for cost used: "+str(epsilon))
+    print("Final Parameters: "+str(theta))
+    return theta
+
+# Initialization for contour and 3d plots
+
+# initalize x1 and x2 based on the range of theta's finally calculated
+
+if not draw_3d:
+    x1 = np.linspace(-1.5, 1.5, 200)
+    y1 = np.linspace(-1.5, 2.5, 200)
+else:
+    x1 = np.linspace(-0.02, 0.02, 100)
+    y1 = np.linspace(-0.75, 1.5, 100)
+    
+
+Xg,Yg = np.meshgrid(x1,y1)
+
+# Zg contains the value of cost for theta = (Xg[i][j],Yg[i][j])
+Zg = []
+
+for i in range(Xg.shape[0]):
+    Zg.append([])
+    for j in range(Xg.shape[1]):
+        Zg[i].append(cost(X,Y,np.array([Xg[i][j],Yg[i][j]])))
+
+Zg = np.array(Zg)
+
+if not draw_3d:
+    ax = plt.figure(2).add_axes([0.2,0.2,1,1])
+    cp = ax.contour(Xg,Yg,Zg)
+    plt.clabel(cp,inline=1,fontsize = 10)
+else:
+    ax = plt.axes(projection='3d')
+    ax.view_init(30, 60)
+    ax.plot_wireframe(Xg,Yg,Zg,color='blue',linewidths=0.3)
+    ax.set_zlabel('Cost')
+    
+
+ax.set_xlabel('Theta[0]')
+ax.set_ylabel('Theta[1]')
+
+theta = train(X,Y,theta)
+
+# 2d plot for data and hypothesis
+plt.figure()
+predictions = np.dot(X,theta)
+plt.scatter(X[:,0],Y,label = "Ground Truth")
+plt.scatter(X[:,0],predictions,label = "Predictions")
+plt.title("Data and hypothesis plot")
+plt.xlabel("Wine Acidity")
+plt.ylabel("Wine Density")
+#plt.show()
+plt.savefig('../plots/1_b.png')
+
+# demo running to check the dimensionality and correctness of various operations in numpy.
+
+#check1 = np.array([[1,2],[2,3]])
+#check2 = np.array([[0],[-1]])
+#print(check1.shape)
+#print(check2.shape)
+#print(np.dot(check1,check2))
---
+++ Q2/Q2.py
@@ -0,0 +1,110 @@
+# Importing libraries..
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# sampling points
+num_samples = 1000000
+sample_theta_array = [[3],[1],[2]]
+sample_theta = np.array(sample_theta_array)
+
+x0 = np.ones((num_samples,1))
+x1 = np.random.normal(3,2,(num_samples,1))
+x2 = np.random.normal(-1,2,(num_samples,1))
+noise = np.random.normal(0,np.sqrt(2),(num_samples,1))
+
+Y = x0*sample_theta[0][0] + x1*sample_theta[1][0] + x2*sample_theta[2][0] + noise
+X = np.append(x0,x1,axis = 1)
+X = np.append(X,x2,axis = 1)
+
+#initializing parameters
+
+alpha = 0.001
+epsilon = 0.001
+max_iter = 10000000000
+theta = np.zeros((sample_theta.size,1))
+batch_size = 1000
+check_size = 2000
+
+cost_list = []
+theta_list = [theta]
+
+# helper functions to compute cost, calculate the gradient and train the model..
+
+def cost(X,Y,theta):
+    temp = np.dot(X,theta)
+    temp = (Y - temp)**2
+    this_cost = np.sum(temp)/(2*X.shape[0])
+    return this_cost
+
+def cost_grad_update(X,Y,theta,alpha):
+    temp = np.dot(X,theta)
+    temp = Y - temp
+    theta = theta + (alpha*np.dot(X.transpose(),temp))/(X.shape[0])
+    return theta
+
+def train(X,Y,theta):
+    ax = plt.axes(projection='3d')
+    iteration = 0
+    finished = False
+    curr_cost = 0
+    old_cost = 0
+    
+    plt.figure()
+    while(not finished):
+        for idx in range(0,num_samples,batch_size):
+            
+            if(finished):
+                break
+                
+            x_curr = X[idx:idx+batch_size,:]
+            y_curr = Y[idx:idx+batch_size,:]
+            
+            curr_cost += cost(x_curr,y_curr,theta)
+            theta = cost_grad_update(x_curr,y_curr,theta,alpha)
+            
+            if(iteration%check_size==0):
+                curr_cost /= check_size
+                cost_list.append(curr_cost)
+                theta_list.append(theta)
+                print("Iteration: "+str(iteration)+" ==> "+ str(abs(curr_cost-old_cost)))
+                if(abs(curr_cost-old_cost)<epsilon or iteration>=max_iter):
+                    finished = True
+                
+                old_cost = curr_cost
+                curr_cost = 0
+            
+            if(iteration%(check_size/10)==0):
+                ax.scatter3D(theta[0],theta[1],theta[2])
+                
+            iteration += 1
+    
+    plt.savefig('../plots/2_d.png')
+    print("Learning rate: "+str(alpha))
+    print("Batch size used: "+str(batch_size))
+    print("Iterations taken: "+str(iteration))
+    print("Epsilon for cost used: "+str(epsilon))
+    print("Final Parameters: "+str(theta))
+    return theta
+
+# training of model
+theta = train(X,Y,theta)
+
+# testing on given input
+
+df = pd.read_csv("/Users/sidharthagarwal/Desktop/assignments/ml774/data/q2/q2test.csv")
+x0 = np.ones((df['X_1'].shape[0],1))
+x1 = np.array(df['X_1']).reshape(-1,1)
+x2 = np.array(df['X_2']).reshape(-1,1)
+
+Y_test = np.array(df['Y']).reshape(-1,1)
+X_test_temp = np.append(x0,x1,axis = 1)
+X_test = np.append(X_test_temp,x2,axis = 1)
+
+error_learned = cost(X_test,Y_test,theta)
+error_original = cost(X_test,Y_test,sample_theta)
+
+print("Error for learned model with batch_size = "+str(batch_size)+" : "+str(error_learned))
+print("Error for original hypthesis : "+str(error_original))
+
